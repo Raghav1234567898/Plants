290e4cea01a6378277353762d7f767d0017fa420
Raghvendra <126984826+Raghav1234567898@users.noreply.github.com>
2023-03-17 20:19:16 +0530

    Add files via upload

---
+++ to.js
@@ -0,0 +1,51 @@
+gsap.from('img', 
+{
+  opacity : 0,
+    duration : 1,
+    x : 200,
+    delay : 1,
+    stagger : 0.6
+})
+gsap.from('h1',{
+  opacity : 0,
+  duration : 1,
+  y : -50,
+  ease :'Power4.easeInout',
+})
+gsap.from('h6',{
+  opacity : 0,
+  duration : 1,
+  y : -50,
+  ease :'Power2.easeInout',
+  stagger : 0.6
+})
+gsap.from('h3',{
+  opacity : 0,
+  duration : 1,
+  y : -50,
+})
+gsap.from('p',{
+  opacity : 0,
+  duration : 1.4,
+  y : -50,
+  stagger : 0.6,
+  delay :1
+})
+gsap.from('a',{
+  opacity : 0,
+  duration : 1.4,
+  y : -50,
+  stagger : 0.6
+})
+gsap.from('li',{
+  opacity : 0,
+  duration : 1.4,
+  y : -50,
+  stagger : 0.6
+})
+gsap.from('i',{
+  opacity : 0,
+  duration : 1,
+  x : -70,
+  stagger : 0.6
+})
